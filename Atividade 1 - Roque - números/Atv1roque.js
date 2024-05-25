10c3a370eba667440f51facac883267580577706
Gabriel Miranda <85796106+Gabrielmira@users.noreply.github.com>
2024-05-25 10:35:22 -0300

    Add files via upload

---
+++ Atividade 1 - Roque - números/Atv1roque.js
@@ -0,0 +1,23 @@
+var num1 = Number(prompt("Digite o primeiro número:"));
+var num2 = Number(prompt("Digite o segundo número:"));
+var num3 = Number(prompt("Digite o terceiro número:"));
+console.log("Primeiro número:", num1);
+console.log("Segundo número:", num2);
+console.log("Terceiro número:", num3);
+
+
+var num1 = Number(prompt("Digite o primeiro número:"));
+var num2 = Number(prompt("Digite o segundo número:"));
+var num3 = Number(prompt("Digite o terceiro número:"));
+var soma = num1 + num2 + num3;
+var media = soma / 3;
+console.log("A soma dos números é:", soma);
+console.log("A média dos números é:", media);
+
+
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+console.log("Primeiro número:", num1);
+console.log("Segundo número:", num2);
+console.log("Terceiro número:", num3);
